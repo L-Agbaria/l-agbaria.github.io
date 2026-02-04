f297f4ec8d71c246946a6c555c101d0ee7de144d
L-Agbaria <163034311+L-Agbaria@users.noreply.github.com>
2026-02-04 19:14:31 +0100

    Update new.js

--- new.js
+++ new.js
@@ -12,6 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
         yesScale += 0.1;
         yesBtn.style.transform = `scale(${yesScale})`;
     });
+    noBtn.addEventListener("click", () => {
+        const x = (Math.random() + 1) * 400 * (Math.random() - 0.5);
+        const y = (Math.random() + 1) * 200 * (Math.random() - 0.5);
+        noBtn.style.transform = `translate(${x}px, ${y}px)`;
+        yesScale += 0.1;
+        yesBtn.style.transform = `scale(${yesScale})`;
+    });
   }
 
   if (yesBtn) {
@@ -20,3 +27,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
